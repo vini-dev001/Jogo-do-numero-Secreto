453289aaa512c0c9c4c1aa7bc306f66cdf27c21d
Vinicius Dolci <viniciusrochadolci.06@gmail.com>
2025-04-07 10:44:42 +0100

    projeto inicial

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -18,7 +18,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do Número Secreto');
-    exibirTextoNaTela('p', 'Bem vindo ao jogo do Numero Secreto!');
+    exibirTextoNaTela('p', 'Escolha um numero de 1 a 100.');
 }
 exibirMensagemInicial();
 
